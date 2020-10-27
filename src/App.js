44b4dfd52516c62864e73d52dc37f9907097f841
Michael <mpgonz@gmail.com>
2020-10-27 18:30:34 -0400

    switching to react page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>hello hello</div>
+        )
+    }
+}
+
+export default App;
